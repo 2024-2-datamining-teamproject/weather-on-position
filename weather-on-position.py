a6f1673ff25fe34e5286ebc8920f4be4819a9881
gurbur <jihwan7577@gmail.com>
2024-11-13 11:28:57 +0900

    Feat: weather code extraction/translation between api and out project

--- weather-on-position.py
+++ weather-on-position.py
@@ -16,14 +16,42 @@ def get_weather_info(lat, lon):
 
     if response.status_code == 200:
         data = response.json()
-        print(data)
-
+        #print(data)
+        #print(data.get('weather'))
+        weather_code = data.get('weather')[0].get('id')
+        return weather_code
     else:
         print("Request failed:", response.status_code);
 
+def translate_weather_code(weather_code):
+    if weather_code == 800:
+        # 맑은 날씨의 경우
+        print(0)
+        return
+    elif weather_code % 100 == 8:
+        # 구름 낀 날씨의 경우
+        print(1)
+        return
+    elif weather_code % 100 == 3 or weather_code % 100 == 4:
+        # 비오는 날씨의 경우
+        print(2)
+        return
+    elif weather_code % 100 == 6:
+        # 눈오는 날씨의 경우
+        print(3)
+        return
+    elif weather_code % 100 == 2 or weather_code == 771 or weather_code == 781:
+        # 폭풍치는 날씨의 경우
+        print(4)
+        return
+    elif weather_code % 100 == 7:
+        # 안개 낀 날씨의 경우
+        print(5)
+        return
+
 def main():
-    get_weather_info(lat, lon)
-    
+    weather_code = get_weather_info(lat, lon)
+    translate_weather_code(weather_code)
 
 if __name__ == "__main__":
     main()
